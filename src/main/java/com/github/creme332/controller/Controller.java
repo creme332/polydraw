550b79888607c38f67798f950639eb8d65ae6022
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-09 21:42:34 +0400

    play splash screen animation

--- src/main/java/com/github/creme332/controller/Controller.java
+++ src/main/java/com/github/creme332/controller/Controller.java
@@ -1,15 +1,49 @@
 package com.github.creme332.controller;
 
+import java.util.Timer;
+import java.util.TimerTask;
+
 import com.github.creme332.view.*;
 
 /**
  * Controls all the logic in the application by linking views and model.
  */
 public class Controller {
-    public Frame frame = new Frame(); // frame of app
+    private Frame frame; // frame of app
+    private MenuBar menuBar;
+    private Canvas canvas;
+
+    private void playStartAnimation() {
+        Timer timer = new Timer();
+        TimerTask showMainScreen;
+        final long animationDuration = 800; // ms
+
+        menuBar.setVisible(false);
+        frame.showSplashScreen();
+
+        // show main screen when timer has elapsed
+        showMainScreen = new TimerTask() {
+            @Override
+            public void run() {
+                menuBar.setVisible(true);
+                frame.showMainScreen();
+                timer.cancel();
+                timer.purge();
+            }
+        };
+        timer.schedule(showMainScreen, animationDuration);
+    }
 
     public Controller() {
-        frame.showFrame();
+        try {
+            menuBar = new MenuBar();
+        } catch (Exception e) {
+            System.out.println(e);
+        }
+        canvas = new Canvas();
+        frame = new Frame(menuBar, canvas);
+
+        playStartAnimation();
     }
 
 }
