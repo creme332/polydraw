7597660bf100f70e3e7c86360824c27fdba8f226
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-09 21:45:13 +0400

    create sample frame

--- src/main/java/com/github/creme332/view/Frame.java
+++ src/main/java/com/github/creme332/view/Frame.java
@@ -15,9 +15,11 @@ public class Frame extends JFrame {
     JPanel screenContainer = new JPanel(); // a container for all screens
     private CardLayout cl = new CardLayout(); // used to swap between screens
 
-    public Frame() {
+    SplashScreen splashScreen = new SplashScreen();
 
-        this.setTitle("polydraw"); // set frame title
+    public Frame(MenuBar menubar, Canvas grid) {
+        // set frame title
+        this.setTitle("polydraw");
 
         // set frame size
         this.setSize(frameWidth, frameHeight);
@@ -28,19 +30,18 @@ public class Frame extends JFrame {
         // add close button to frame
         this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
-        // ! Maximize frame on startup may cause a white screen to appear.
-        // this.setExtendedState(JFrame.MAXIMIZED_BOTH); // maximize frame on startup
-
         // center frame on startup if frame is not maximized
         if (this.getExtendedState() != JFrame.MAXIMIZED_BOTH) {
-            // ! Do not set locationRelative to null if screen is maximized
             this.setLocationRelativeTo(null);
         }
 
         screenContainer.setLayout(cl);
-        screenContainer.add(new JColorChooser());
-
+        screenContainer.add(splashScreen, "splashScreen");
+        screenContainer.add(grid, "canvas");
         this.add(screenContainer);
+
+        setJMenuBar(menubar);
+        this.setVisible(true);
     }
 
     /**
@@ -49,7 +50,11 @@ public class Frame extends JFrame {
      * Call this function once all components have been added to the frame
      * to ensure proper rendering.
      */
-    public void showFrame() {
-        this.setVisible(true);
+    public void showMainScreen() {
+        cl.show(screenContainer, "canvas");
+    }
+
+    public void showSplashScreen() {
+        cl.show(screenContainer, "splashScreen");
     }
 }
