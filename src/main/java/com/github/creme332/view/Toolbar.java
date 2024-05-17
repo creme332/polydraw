3a5ebc868a768de93e98037a3131553a7fe253d4
creme332 <65414576+creme332@users.noreply.github.com>
2024-05-17 21:24:19 +0400

    add color picker

--- src/main/java/com/github/creme332/view/Toolbar.java
+++ src/main/java/com/github/creme332/view/Toolbar.java
@@ -1,11 +1,17 @@
 package com.github.creme332.view;
 
+import java.awt.Color;
 import java.awt.Dimension;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JColorChooser;
 import javax.swing.JMenu;
 import javax.swing.JMenuBar;
 import javax.swing.JMenuItem;
+import javax.swing.JPanel;
 import javax.swing.JSlider;
 import javax.swing.JToolBar;
 import javax.swing.border.Border;
@@ -13,13 +19,15 @@ import javax.swing.border.EtchedBorder;
 
 import com.github.creme332.utils.IconLoader;
 
-public class Toolbar extends JToolBar {
+public class Toolbar extends JToolBar implements ActionListener {
 
     static final int FPS_MIN = 1;
     static final int FPS_MAX = 13;
     static final int FPS_INIT = 1; // initial frames per second
     JSlider framesPerSecond = new JSlider(JSlider.HORIZONTAL,
             FPS_MIN, FPS_MAX, FPS_INIT);
+
+    private JButton colorBox;
 
     public Toolbar() throws Exception {
 
@@ -57,27 +65,31 @@ public class Toolbar extends JToolBar {
 
         menuBar.add(menu);
 
-        menu = new JMenu();
-        menu.setToolTipText("Set color");
+        // color picker menu
+        JPanel aaa = new JPanel();
+        colorBox = new JButton();
+        colorBox.setBorderPainted(false);
+        colorBox.setBackground(Color.black);
+        colorBox.setPreferredSize(new Dimension(50, 50));
+        colorBox.setToolTipText("Set color");
+        aaa.add(colorBox);
+        menuBar.add(aaa);
 
-        menu.setIcon(loader.loadIcon("/icons/solid-line.png", 50));
-
-        menuItem = new JMenuItem("Solid Line",
-                loader.loadIcon("/icons/solid-line.png", 50));
-        menu.add(menuItem);
-
-        menuItem = new JMenuItem("Dashed Line",
-                loader.loadIcon("/icons/dashed-line.png", 50));
-        menu.add(menuItem);
-
-        menuItem = new JMenuItem("Dotted Line",
-                loader.loadIcon("/icons/dotted-line.png", 50));
-        menu.add(menuItem);
-
-        menuBar.add(menu);
+        colorBox.addActionListener(this);
 
         this.add(menuBar);
 
+    }
+
+    public void actionPerformed(ActionEvent e) {
+        System.out.println("Choose color");
+
+        // color chooser Dialog Box
+        Color color = JColorChooser.showDialog(this,
+                "Select a color", Color.black);
+
+        // set Background color of the Container
+        colorBox.setBackground(color);
     }
 
 }
