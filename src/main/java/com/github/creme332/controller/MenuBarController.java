67a1b776795cfc1eb4c71168e523809c2bb57eef
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-01 14:55:04 +0400

    create component controllers

---
+++ src/main/java/com/github/creme332/controller/MenuBarController.java
@@ -0,0 +1,16 @@
+package com.github.creme332.controller;
+
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+
+import com.github.creme332.view.MenuBar;
+
+public class MenuBarController {
+    private MenuBar menu;
+
+    public MenuBarController(MenuBar menu) {
+        this.menu = menu;
+
+        // TODO: add action listener to menu
+    }
+}
---
+++ src/main/java/com/github/creme332/controller/ToolBarController.java
@@ -0,0 +1,11 @@
+package com.github.creme332.controller;
+
+import com.github.creme332.view.Toolbar;
+
+public class ToolBarController {
+    private Toolbar toolbar;
+
+    public ToolBarController(Toolbar toolbar) {
+        this.toolbar = toolbar;
+    }
+}
