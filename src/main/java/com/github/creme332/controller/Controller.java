1d7a150652e91e1c0cd816c354b38fb8514bf14b
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-01 14:55:23 +0400

    use component controllers

--- src/main/java/com/github/creme332/controller/Controller.java
+++ src/main/java/com/github/creme332/controller/Controller.java
@@ -1,7 +1,5 @@
 package com.github.creme332.controller;
 
-import java.util.Timer;
-import java.util.TimerTask;
 
 import com.github.creme332.utils.exception.InvalidIconSizeException;
 import com.github.creme332.utils.exception.InvalidPathException;
@@ -16,57 +14,40 @@ public class Controller {
     private Canvas canvas;
     private Toolbar toolbar;
 
+    private FrameController frameController;
     private CanvasController canvasController;
-
-    private void playStartAnimation() {
-        Timer timer = new Timer();
-        TimerTask showMainScreen;
-        final long animationDuration = 800; // ms
-
-        menuBar.setVisible(false);
-        frame.showSplashScreen();
-
-        // show main screen when timer has elapsed
-        showMainScreen = new TimerTask() {
-            @Override
-            public void run() {
-                menuBar.setVisible(true);
-                frame.showMainScreen();
-                timer.cancel();
-                timer.purge();
-            }
-        };
-        timer.schedule(showMainScreen, animationDuration);
-    }
+    private MenuBarController menuController;
+    private SideMenuController sideController;
+    private ToolBarController toolbarController;
 
     public Controller() {
         try {
             toolbar = new Toolbar();
-            menuBar = new MenuBar();
+            toolbarController = new ToolBarController(toolbar);
 
             canvas = new Canvas(toolbar);
             canvasController = new CanvasController(canvas);
 
+            frame = new Frame(canvas);
+            frameController = new FrameController(frame);
 
-            frame = new Frame(menuBar, canvas);
-            frame.showMainScreen();
+            menuBar = frame.getMyMenuBar();
+            menuController = new MenuBarController(menuBar);
 
+            sideController = new SideMenuController(frame.getSideMenuPanel());
         } catch (InvalidIconSizeException e) {
-            // Handle invalid icon size
             System.err.println("Error: " + e.getMessage());
             System.exit(1);
         } catch (InvalidPathException e) {
-            // Handle invalid path
             System.err.println("Error: " + e.getMessage());
             System.exit(1);
         } catch (Exception e) {
-            // Handle any other unexpected exceptions
             System.err.println("Unexpected error: " + e.getMessage());
             e.printStackTrace();
             System.exit(1);
         }
 
-        playStartAnimation();
+        frameController.playStartAnimation();
     }
 
 }
