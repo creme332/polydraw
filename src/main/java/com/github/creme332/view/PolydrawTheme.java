0ba19d3913049e321531c16141ce40151d318c84
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-06 21:53:17 +0400

    do not embed menu bar into the window title pane

--- src/main/java/com/github/creme332/view/PolydrawTheme.java
+++ src/main/java/com/github/creme332/view/PolydrawTheme.java
@@ -4,8 +4,12 @@ import com.formdev.flatlaf.FlatLightLaf;
 
 public class PolydrawTheme extends FlatLightLaf {
     public static boolean setup() {
+        // do not embed menu bar into the window title pane
+        System.setProperty("flatlaf.menuBarEmbedded", "false");
+
         // look for properties file in theme folder (relative to resource)
         registerCustomDefaultsSource("theme");
+
         return setup(new PolydrawTheme());
     }
 
