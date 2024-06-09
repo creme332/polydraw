3d99658ac97e8a3a838ca4a0ada7edf3673d791a
triya <triya.sookorry@umail.uom.ac.mu>
2024-06-09 17:17:52 +0300

    2 changes.No test yet

---
+++ src/main/algorithms/LineAlgorithm.java
@@ -0,0 +1,8 @@
+public class LineAlgorithm {
+    public static int[][] drawDDA(int x0, int y0, int x1, int y1) {
+        return new int[0][0];
+    }
+    public static int[][] drawBresenham(int x0, int y0, int x1, int y1) {
+        return new int[0][0];
+    }
+}
