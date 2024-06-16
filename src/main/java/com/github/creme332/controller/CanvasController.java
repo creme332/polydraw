56b4c79ac415ada8ab7d4831f5f0ca7389a4be66
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-16 15:49:38 +0400

    implement draw dynamic circle

--- src/main/java/com/github/creme332/controller/CanvasController.java
+++ src/main/java/com/github/creme332/controller/CanvasController.java
@@ -7,10 +7,12 @@ import java.awt.geom.Point2D;
 import java.beans.PropertyChangeEvent;
 import java.beans.PropertyChangeListener;
 import java.awt.Point;
+import java.awt.Polygon;
 import java.awt.event.ComponentAdapter;
 import java.awt.event.ComponentEvent;
 import java.awt.event.MouseAdapter;
 
+import com.github.creme332.algorithms.CircleAlgorithm;
 import com.github.creme332.model.AppState;
 import com.github.creme332.model.CanvasModel;
 import com.github.creme332.model.Mode;
@@ -20,6 +22,7 @@ import com.github.creme332.view.Canvas;
 public class CanvasController implements PropertyChangeListener {
     private Canvas canvas;
 
+    private CircleAlgorithm circleCalculator = new CircleAlgorithm();
     /**
      * Used to store coordinate where mouse drag started
      */
@@ -66,6 +69,24 @@ public class CanvasController implements PropertyChangeListener {
                             polySpaceMousePosition);
                     currentWrapper.setShape(line);
                     canvas.repaint();
+                    return;
+                }
+                if (app.getMode() == Mode.DRAW_CIRCLE_DYNAMIC && currentWrapper != null
+                        && currentWrapper.getPlottedPoints().size() == 1) {
+
+                    Point2D center = currentWrapper.getPlottedPoints().get(0);
+                    double radius = polySpaceMousePosition.distance(center);
+                    int roundedRadius = (int) Math.round(radius);
+                    if (roundedRadius == 0)
+                        return;
+
+                    // System.out.format("plotting center center (%d, %d) radius %d %n", (int) center.getX(),
+                    //         (int) center.getY(), roundedRadius);
+
+                    currentWrapper.setShape(getCircle((int) center.getX(), (int) center.getY(), roundedRadius));
+                    canvas.repaint();
+                    return;
+
                 }
 
             }
@@ -94,6 +115,11 @@ public class CanvasController implements PropertyChangeListener {
     private void handleCanvasZoom(MouseWheelEvent e) {
         model.updateCanvasZoom(e.getWheelRotation() != 1);
         canvas.repaint();
+    }
+
+    private Polygon getCircle(int x, int y, int radius) {
+        int[][] coordinates = circleCalculator.calculateOrderedPoints(x, y, radius);
+        return new Polygon(coordinates[0], coordinates[1], coordinates[0].length);
     }
 
     private void handleMouseDragged(MouseEvent e) {
@@ -130,15 +156,15 @@ public class CanvasController implements PropertyChangeListener {
     }
 
     private void handleMousePressed(MouseEvent e) {
-        if (app.getMode() == Mode.MOVE_GRAPHICS_VIEW || app.getMode() == Mode.MOVE_CANVAS) {
-            mouseDragStart = e.getPoint();
-        }
-
         /**
          * Coordinates of mouse pressed in the polydraw coordinate system
          */
         Point2D polySpaceMousePosition = model.toPolySpace(e.getPoint());
-        System.out.println(polySpaceMousePosition);
+
+        if (app.getMode() == Mode.MOVE_GRAPHICS_VIEW || app.getMode() == Mode.MOVE_CANVAS) {
+            mouseDragStart = e.getPoint();
+            return;
+        }
 
         if (app.getMode() == Mode.DRAW_LINE_BRESENHAM || app.getMode() == Mode.DRAW_LINE_DDA) {
             if (currentWrapper == null) {
@@ -165,7 +191,63 @@ public class CanvasController implements PropertyChangeListener {
 
                 currentWrapper = null;
             }
+        }
 
+        if (app.getMode() == Mode.DRAW_CIRCLE_DYNAMIC) {
+            if (currentWrapper == null) {
+                // center has been selected
+
+                // create a shape wrapper
+                currentWrapper = new ShapeWrapper();
+                currentWrapper.getPlottedPoints().add(polySpaceMousePosition);
+
+                // save wrapper
+                model.getShapes().add(currentWrapper);
+
+            } else {
+                // second point has now been selected
+                currentWrapper.getPlottedPoints().add(polySpaceMousePosition);
+
+                // create a circle
+                Point2D center = currentWrapper.getPlottedPoints().get(0);
+                double radius = polySpaceMousePosition.distance(center);
+                int roundedRadius = (int) Math.round(radius);
+                if (roundedRadius == 0)
+                    return;
+
+                currentWrapper.setShape(getCircle((int) center.getX(), (int) center.getY(), roundedRadius));
+
+                currentWrapper = null;
+            }
+        }
+
+        if (app.getMode() == Mode.DRAW_ELLIPSE) {
+            if (currentWrapper == null) {
+                // first foci has been selected
+
+                // create a shape wrapper
+                currentWrapper = new ShapeWrapper();
+                currentWrapper.getPlottedPoints().add(polySpaceMousePosition);
+
+                // save wrapper
+                model.getShapes().add(currentWrapper);
+
+            } else {
+                // second foci has now been selected
+                if (currentWrapper.getPlottedPoints().size() == 1) {
+                    currentWrapper.getPlottedPoints().add(polySpaceMousePosition);
+                } else {
+                    // third point has been selected
+                    currentWrapper.getPlottedPoints().add(polySpaceMousePosition);
+
+                    // create an ellipse
+                    Polygon ellipse = new Polygon();
+
+                    currentWrapper.setShape(ellipse);
+
+                    currentWrapper = null;
+                }
+            }
         }
 
         canvas.repaint();
