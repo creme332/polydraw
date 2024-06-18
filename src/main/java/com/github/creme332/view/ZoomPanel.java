fb3267ba7a0c48214d7fd24e3d152bedcdfd09fa
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-18 10:32:36 +0400

    create zoom panel

---
+++ src/main/java/com/github/creme332/view/ZoomPanel.java
@@ -0,0 +1,87 @@
+package com.github.creme332.view;
+
+import java.awt.Color;
+import java.awt.Dimension;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.Insets;
+import java.awt.event.ComponentAdapter;
+import java.awt.event.ComponentEvent;
+
+import javax.swing.JButton;
+import javax.swing.JPanel;
+
+import org.kordamp.ikonli.Ikon;
+import org.kordamp.ikonli.bootstrapicons.BootstrapIcons;
+import org.kordamp.ikonli.swing.FontIcon;
+
+public class ZoomPanel extends JPanel {
+
+    private JButton homeButton = new CircularButton();
+    private JButton zoomInButton = new CircularButton();
+    private JButton zoomOutButton = new CircularButton();
+
+    public ZoomPanel() {
+        setOpaque(false);
+        this.setLayout(new GridBagLayout());
+        GridBagConstraints gbc = new GridBagConstraints();
+
+        // create buttons for zoom panel
+        homeButton = createZoomPanelButton(BootstrapIcons.HOUSE);
+        zoomInButton = createZoomPanelButton(BootstrapIcons.ZOOM_IN);
+        zoomOutButton = createZoomPanelButton(BootstrapIcons.ZOOM_OUT);
+
+        gbc.fill = GridBagConstraints.HORIZONTAL;
+        gbc.insets = new Insets(20, 0, 0, 0);
+        gbc.gridx = 0;
+        gbc.gridy = 0;
+        gbc.gridwidth = 1;
+
+        this.add(homeButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        this.add(zoomInButton, gbc);
+
+        gbc.gridx = 0;
+        gbc.gridy = 2;
+        this.add(zoomOutButton, gbc);
+
+        addComponentListener(new ComponentAdapter() {
+            @Override
+            public void componentResized(ComponentEvent e) {
+                // get current dimensions
+                int width = getWidth();
+                int height = getHeight();
+
+                System.out.format("Zoom panel dimensions = %d x %d %n", width, height);
+            }
+        });
+    }
+
+    public JButton createZoomPanelButton(Ikon ikon) {
+        final int ICON_SIZE = 30;
+        final int BUTTON_SIZE = 60;
+        final Color gray = new Color(116, 116, 116);
+
+        JButton btn = new CircularButton();
+        btn.setPreferredSize(new Dimension(BUTTON_SIZE, BUTTON_SIZE));
+
+        FontIcon icon = FontIcon.of(ikon, ICON_SIZE);
+        icon.setIconColor(gray);
+        btn.setIcon(icon);
+        return btn;
+    }
+
+    public JButton getHomeButton() {
+        return homeButton;
+    }
+
+    public JButton getZoomInButton() {
+        return zoomInButton;
+    }
+
+    public JButton getZoomOutButton() {
+        return zoomOutButton;
+    }
+}
