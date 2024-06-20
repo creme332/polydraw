35b974ac31173e9ca9230ee79e591d8b618a006b
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-20 17:35:52 +0400

    create a back button

---
+++ src/main/java/com/github/creme332/view/BackButton.java
@@ -0,0 +1,17 @@
+package com.github.creme332.view;
+
+import javax.swing.JButton;
+
+import org.kordamp.ikonli.bootstrapicons.BootstrapIcons;
+import org.kordamp.ikonli.swing.FontIcon;
+
+public class BackButton extends JButton {
+    public BackButton() {
+        setOpaque(false);
+        setBorderPainted(false);
+        setFocusPainted(false);
+        FontIcon icon = FontIcon.of(BootstrapIcons.ARROW_LEFT_CIRCLE);
+        icon.setIconSize(40);
+        setIcon(icon);
+    }
+}
