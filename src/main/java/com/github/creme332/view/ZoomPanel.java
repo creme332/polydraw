08665fe9ca1e5ea3b15e0a0e91d266b9e5a1e0ab
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-22 18:48:10 +0400

    add tootips to each button

--- src/main/java/com/github/creme332/view/ZoomPanel.java
+++ src/main/java/com/github/creme332/view/ZoomPanel.java
@@ -29,6 +29,11 @@ public class ZoomPanel extends JPanel {
         zoomInButton = createZoomPanelButton(BootstrapIcons.ZOOM_IN);
         zoomOutButton = createZoomPanelButton(BootstrapIcons.ZOOM_OUT);
 
+        // add tooltips to zoom panel buttons
+        homeButton.setToolTipText("Reset zoom");
+        zoomInButton.setToolTipText("Zoom in");
+        zoomOutButton.setToolTipText("Zoom out button");
+
         gbc.fill = GridBagConstraints.HORIZONTAL;
         gbc.insets = new Insets(20, 0, 0, 0);
         gbc.gridx = 0;
