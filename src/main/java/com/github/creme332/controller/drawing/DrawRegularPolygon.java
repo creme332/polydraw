066bfca6a4cb508650027d9d73e4ecac74252c95
divyesh000 <divjok28@outlook.com>
2024-06-25 08:57:05 +0400

    complete draw regular polygon controller

--- src/main/java/com/github/creme332/controller/drawing/DrawRegularPolygon.java
+++ src/main/java/com/github/creme332/controller/drawing/DrawRegularPolygon.java
@@ -2,7 +2,11 @@ package com.github.creme332.controller.drawing;
 
 import java.awt.Polygon;
 import java.awt.geom.Point2D;
-
+import javax.swing.JDialog;
+import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.JOptionPane;
+import javax.swing.JTextField;
 import com.github.creme332.algorithms.PolygonCalculator;
 import com.github.creme332.model.AppState;
 import com.github.creme332.model.Mode;
@@ -12,9 +16,11 @@ import com.github.creme332.view.Canvas;
 public class DrawRegularPolygon extends DrawController {
 
     PolygonCalculator calculator = new PolygonCalculator();
+    private int numSides = 5; // default to 5 sides (pentagon)
 
     public DrawRegularPolygon(AppState app, Canvas canvas) {
         super(app, canvas);
+        numSides = getNumSidesFromUser();
     }
 
     @Override
@@ -24,7 +30,7 @@ public class DrawRegularPolygon extends DrawController {
             Point2D center = preview.getPlottedPoints().get(0);
             int side = (int) Math.abs(center.distance(polySpaceMousePosition));
 
-            int[][] res = calculator.getOrderedPoints(5, side, (int) center.getX(), (int) center.getY());
+            int[][] res = calculator.getOrderedPoints(numSides, side, (int) center.getX(), (int) center.getY());
             Polygon p = new Polygon(res[0], res[1], res[0].length);
             preview.setShape(p);
             canvas.repaint();
@@ -50,5 +56,23 @@ public class DrawRegularPolygon extends DrawController {
     @Override
     public boolean shouldDraw() {
         return getCanvasMode() == Mode.DRAW_REGULAR_POLYGON;
+    }
+
+    private int getNumSidesFromUser() {
+        JTextField numSidesField = new JTextField(5);
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("Enter number of sides:"));
+        panel.add(numSidesField);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "Number of Sides", JOptionPane.OK_CANCEL_OPTION,
+                JOptionPane.PLAIN_MESSAGE);
+        if (result == JOptionPane.OK_OPTION) {
+            try {
+                return Integer.parseInt(numSidesField.getText());
+            } catch (NumberFormatException e) {
+                JOptionPane.showMessageDialog(null, "Invalid input. Defaulting to 5 sides.");
+            }
+        }
+        return 5; // default to 5 sides if input is invalid or cancelled
     }
 }
