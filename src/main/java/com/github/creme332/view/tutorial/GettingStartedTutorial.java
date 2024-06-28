6c86e7b1a134099a1ff2a82198e1442e25203836
creme332 <65414576+creme332@users.noreply.github.com>
2024-06-28 16:17:32 +0400

    refactor tutorial and use better images

--- src/main/java/com/github/creme332/view/tutorial/GettingStartedTutorial.java
+++ src/main/java/com/github/creme332/view/tutorial/GettingStartedTutorial.java
@@ -5,6 +5,7 @@ import static com.github.creme332.utils.IconLoader.getScaledDimension;
 
 import java.awt.*;
 import javax.swing.*;
+import javax.swing.border.EmptyBorder;
 import javax.swing.table.DefaultTableCellRenderer;
 import javax.swing.table.DefaultTableModel;
 import org.kordamp.ikonli.bootstrapicons.BootstrapIcons;
@@ -20,16 +21,15 @@ public class GettingStartedTutorial extends TutorialPanel {
     private static final String IMAGE_PATH_PREFIX = "/images/tutorials/getting-started/";
     private static final TutorialModel GETTING_STARTED_MODEL = new TutorialModel("Getting Started");
 
+    GridBagConstraints gbc = new GridBagConstraints();
+    JPanel mainPanel = new JPanel(new GridBagLayout());
+
     public GettingStartedTutorial() throws InvalidPathException, InvalidIconSizeException {
         super(GETTING_STARTED_MODEL, loadIcon(IMAGE_PATH_PREFIX + "background.png",
                 getScaledDimension(new Dimension(753, 453), TutorialCard.IMAGE_DIMENSION)));
         // remove bodyPanel since default layout is not being used
         this.remove(bodyPanel);
 
-        // Create a main panel to hold all content with scrollable area
-        JPanel mainPanel = new JPanel(new GridBagLayout());
-
-        GridBagConstraints gbc = new GridBagConstraints();
         gbc.insets = new Insets(10, 10, 10, 10);
         gbc.anchor = GridBagConstraints.NORTHWEST;
         gbc.fill = GridBagConstraints.HORIZONTAL;
@@ -38,23 +38,37 @@ public class GettingStartedTutorial extends TutorialPanel {
 
         // Add Introduction
         JLabel introLabel = new JLabel(
-                "<html><h1>Getting Started Tutorial</h1><p>Welcome to Polydraw! This tutorial will guide you through the various components and features of the interface to help you get started.</p></html>");
+                "<html><p>Welcome to Polydraw! This tutorial will guide you through the various components of the interface to help you get started.</p></html>");
         mainPanel.add(introLabel, gbc);
         gbc.gridy++;
 
-        // Add ZoomPanel explanation
-        JTextArea zoomPanelExplanation = createSectionText("Zoom Panel",
-                "The zoom panel is located on the right side of the application and allows you to control the zoom level and view of the canvas. The buttons include:");
-        mainPanel.add(zoomPanelExplanation, gbc);
+        addMenubarSection();
+        addZoomPanelSection();
+        addToastSection();
+
+        // Make mainPanel scrollable
+        JScrollPane scrollPane = new JScrollPane(mainPanel,
+                javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
+                javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setBorder(BorderFactory.createEmptyBorder());
+        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
+
+        this.add(scrollPane, BorderLayout.CENTER);
+    }
+
+    private void addZoomPanelSection() throws InvalidPathException {
+        // Add Zoom Panel section title
+        mainPanel.add(createSectionTitle("Zoom Panel"), gbc);
         gbc.gridy++;
 
-        // Add ZoomPanel example image
-        JLabel zoomPanelImageLabel = new JLabel("Here is an example of what the Zoom Panel looks like on the Canvas:");
-        mainPanel.add(zoomPanelImageLabel, gbc);
-        gbc.gridy++;
-        ImageIcon zoomPanelIcon = loadIcon(IMAGE_PATH_PREFIX + "zoom-panel.png");
-        JLabel zoomPanelImage = new JLabel(zoomPanelIcon);
+        // add image
+        JLabel zoomPanelImage = new JLabel(loadIcon(IMAGE_PATH_PREFIX + "zoom-panel.png"));
         mainPanel.add(zoomPanelImage, gbc);
+        gbc.gridy++;
+
+        JTextArea zoomPanelExplanation = createParagraph(
+                "The zoom panel is located on the bottom right side of the screen and allows you to control the zoom level of the canvas.");
+        mainPanel.add(zoomPanelExplanation, gbc);
         gbc.gridy++;
 
         // Add ZoomPanel buttons as a table
@@ -66,24 +80,39 @@ public class GettingStartedTutorial extends TutorialPanel {
         });
         mainPanel.add(zoomPanelTable, gbc);
         gbc.gridy++;
+    }
 
-        // Add Toast explanation
-        JTextArea toastExplanation = createSectionText("Toast Notifications",
-                "Toast notifications provide quick information about the current mode or action being performed. The toast includes a title and instructions, helping you understand what is expected at each step.");
+    private void addToastSection() throws InvalidPathException {
+        // add title
+        mainPanel.add(createSectionTitle("Toast"), gbc);
+        gbc.gridy++;
+
+        // add image
+        mainPanel.add(new JLabel(loadIcon(IMAGE_PATH_PREFIX + "toast.png")), gbc);
+        gbc.gridy++;
+
+        // add description
+        JTextArea toastExplanation = createParagraph(
+                "The toast is a temporary panel that appears on the bottom left side of the screen when you select a new drawing mode. It provides quick instructions about the current mode.");
         mainPanel.add(toastExplanation, gbc);
         gbc.gridy++;
+    }
 
-        // Add Toast example image
-        JLabel toastImageLabel = new JLabel("Here is an example of what a toast looks like on the Canvas:");
-        mainPanel.add(toastImageLabel, gbc);
-        gbc.gridy++;
-        ImageIcon toastIcon = loadIcon(IMAGE_PATH_PREFIX + "toast.png");
-        JLabel toastImage = new JLabel(toastIcon);
-        mainPanel.add(toastImage, gbc);
+    private void addMenubarSection() throws InvalidPathException {
+        // section title
+        mainPanel.add(createSectionTitle("Menubar"), gbc);
         gbc.gridy++;
 
-        // Add Toast explanation as a table
-        JPanel toastTable = createTextTable(new Object[][] {
+        // menubar image
+        mainPanel.add(new JLabel(loadIcon(IMAGE_PATH_PREFIX + "menubar.png")), gbc);
+        gbc.gridy++;
+
+        JTextArea buttonExplanation = createParagraph(
+                "The menubar contains the commonly used buttons which enable you to quickly change the drawing mode. There are several buttons available in the interface for various actions on the right side of the Menu Bar:");
+        mainPanel.add(buttonExplanation, gbc);
+        gbc.gridy++;
+
+        JPanel modesTable = createTextTable(new Object[][] {
                 { "Mode", "Drag or select object" },
                 { "Freehand Shape", "Sketch a function or geometric object" },
                 { "Line: DDA", "Select two points or positions" },
@@ -104,15 +133,9 @@ public class GettingStartedTutorial extends TutorialPanel {
                 { "Normal Rotation", "Select object to rotate" },
                 { "Scaling", "Select object to scale then enter scaling factor" },
                 { "Shear", "Select object to translate, then enter scaling factor" },
-                { "Clipping", "Draw clipping region with mouse drag" }
+                { "Clip", "Draw clipping region with mouse drag" }
         });
-        mainPanel.add(toastTable, gbc);
-        gbc.gridy++;
-
-        // Add Detailed Button explanations
-        JTextArea buttonExplanation = createSectionText("Buttons on the right side",
-                "There are several buttons available in the interface for various actions on the right side of the Menu Bar:");
-        mainPanel.add(buttonExplanation, gbc);
+        mainPanel.add(modesTable, gbc);
         gbc.gridy++;
 
         // Add action buttons as a table
@@ -123,20 +146,19 @@ public class GettingStartedTutorial extends TutorialPanel {
                 { FontIcon.of(BootstrapIcons.ARROW_CLOCKWISE, 40), "Redo", "Redo the last undone action." }
         });
         mainPanel.add(actionButtonsTable, gbc);
-
-        // Add scrollable area with faster scrolling
-        JScrollPane scrollPane = new JScrollPane(mainPanel,
-                javax.swing.ScrollPaneConstants.VERTICAL_SCROLLBAR_AS_NEEDED,
-                javax.swing.ScrollPaneConstants.HORIZONTAL_SCROLLBAR_AS_NEEDED);
-        scrollPane.setBorder(BorderFactory.createEmptyBorder());
-        scrollPane.getVerticalScrollBar().setUnitIncrement(16);
-
-        this.add(scrollPane, BorderLayout.CENTER);
+        gbc.gridy++;
     }
 
-    private JTextArea createSectionText(String title, String content) {
+    private JLabel createSectionTitle(String title) {
+        JLabel titleLabel = new JLabel(title);
+        titleLabel.setBorder(new EmptyBorder(new Insets(20, 0, 0, 0)));
+        titleLabel.putClientProperty("FlatLaf.style", "font: bold $h3.font");
+        return titleLabel;
+    }
+
+    private JTextArea createParagraph(String content) {
         JTextArea textArea = new JTextArea();
-        textArea.setText(title + "\n\n" + content);
+        textArea.setText(content);
         textArea.setWrapStyleWord(true);
         textArea.setLineWrap(true);
         textArea.setEditable(false);
