9f528cb91c5ac157bc2ca1466b7697339006724b
creme332 <65414576+creme332@users.noreply.github.com>
2024-07-01 16:18:13 +0400

    update eliipse tutorial

--- src/main/java/com/github/creme332/view/tutorial/DrawEllipseTutorial.java
+++ src/main/java/com/github/creme332/view/tutorial/DrawEllipseTutorial.java
@@ -1,7 +1,6 @@
 package com.github.creme332.view.tutorial;
 
 import static com.github.creme332.utils.IconLoader.loadIcon;
-import static com.github.creme332.utils.IconLoader.getScaledDimension;
 
 import java.awt.Dimension;
 
@@ -16,17 +15,16 @@ import com.github.creme332.utils.exception.InvalidPathException;
 public class DrawEllipseTutorial extends TutorialPanel {
 
     private static final String IMAGE_PATH_PREFIX = "/images/tutorials/draw-ellipse/";
-    private static final TutorialModel DRAW_CIRCLE_MODEL = new TutorialModel("Draw Ellipse");
+    private static final TutorialModel TUTORIAL_MODEL = new TutorialModel("Draw Ellipse");
 
     public DrawEllipseTutorial() throws InvalidPathException, InvalidIconSizeException {
-        super(DRAW_CIRCLE_MODEL, loadIcon(IMAGE_PATH_PREFIX + "background.png",
-                getScaledDimension(new Dimension(1291, 616), TutorialCard.IMAGE_DIMENSION)));
+        super(TUTORIAL_MODEL, loadIcon(IMAGE_PATH_PREFIX + "background.png", TutorialCard.IMAGE_DIMENSION));
         model.addKeyword("ellipse");
 
         try {
             // Insert text
             doc.insertString(doc.getLength(),
-                    "In this tutorial you will learn how to draw an Ellipse using an ellipse algorithm.\n\n",
+                    "In this tutorial you will learn how to draw an Ellipse using the Midpoint Ellipse Algorithm.\n\n",
                     regular);
 
             ImageIcon icon = loadIcon(IMAGE_PATH_PREFIX + "draw-ellipse.gif", new Dimension(1000, 593));
@@ -34,10 +32,7 @@ public class DrawEllipseTutorial extends TutorialPanel {
             doc.insertString(doc.getLength(), " ", imageStyle);
 
             doc.insertString(doc.getLength(),
-                    "\n\n1. Click on the line icon in the menu bar.\n",
-                    regular);
-            doc.insertString(doc.getLength(),
-                    "\n\n2. Select the ellipse algorithm that you want to use in the expanded menu.\n\n",
+                    "\n\n1. Click on the ellipse icon in the menu bar to select the ellipse drawing mode.\n",
                     regular);
 
             icon = loadIcon(IMAGE_PATH_PREFIX + "step-2.png");
@@ -45,10 +40,14 @@ public class DrawEllipseTutorial extends TutorialPanel {
             doc.insertString(doc.getLength(), " ", imageStyle);
 
             doc.insertString(doc.getLength(),
-                    "\n\n3. Move your mouse cursor on the canvas and click on a point where you want your line to start then a second point.\n",
+                    "\n\n2. Move your mouse cursor on the canvas and click on a point where you want your first ellipse focus to be.\n",
                     regular);
             doc.insertString(doc.getLength(),
-                    "\n\n4. Click on another point where you want your line to end.\n\n",
+                    "\n\n3. Click on another point where you want your second ellipse focus to be.\n\n",
+                    regular);
+
+            doc.insertString(doc.getLength(),
+                    "\n\n4. At this point, you can move your cursor to control the size of the ellipse. When you are done, click on a third point to complete your ellipse.\n\n",
                     regular);
 
             icon = loadIcon(IMAGE_PATH_PREFIX + "step-3.png");
