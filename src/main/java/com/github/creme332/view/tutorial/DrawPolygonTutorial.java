573f5c43995b2aae4bf555657ec4e5ee07932141
creme332 <65414576+creme332@users.noreply.github.com>
2024-07-02 18:18:16 +0400

    add  headings, comment out invalid images

--- src/main/java/com/github/creme332/view/tutorial/DrawPolygonTutorial.java
+++ src/main/java/com/github/creme332/view/tutorial/DrawPolygonTutorial.java
@@ -17,29 +17,34 @@ public class DrawPolygonTutorial extends TutorialPanel {
 
     private static final String IMAGE_PATH_PREFIX = "/images/tutorials/draw-polygon/";
     private static final TutorialModel TUTORIAL_MODEL = new TutorialModel("Draw Polygon");
+    ImageIcon icon;
 
     public DrawPolygonTutorial() throws InvalidPathException, InvalidIconSizeException {
         super(TUTORIAL_MODEL, loadSVGIcon("/icons/regular-polygon.svg", TutorialCard.IMAGE_DIMENSION));
         model.addKeyword("regular");
         model.addKeyword("irregular");
 
-                try {
-            // Insert text
+        try {
             doc.insertString(doc.getLength(),
-                    "In this tutorial you will learn how to draw an Polygon using the Regular and Irregular Polygon Algorithm.\n\n",
+                    "In this tutorial you will learn how to draw regular and irregular polygons.\n\n",
                     regular);
 
-            ImageIcon icon = loadIcon(IMAGE_PATH_PREFIX + "draw-polygon.gif", new Dimension(1000, 593));
-            StyleConstants.setIcon(imageStyle, icon);
-            doc.insertString(doc.getLength(), " ", imageStyle);
+            // ImageIcon icon = loadIcon(IMAGE_PATH_PREFIX + "draw-polygon.gif", new
+            // Dimension(1000, 593));
+            // StyleConstants.setIcon(imageStyle, icon);
+            // doc.insertString(doc.getLength(), " ", imageStyle);
+
+            doc.insertString(doc.getLength(),
+                    "Draw regular polygon\n",
+                    boldLarge);
 
             doc.insertString(doc.getLength(),
                     "\n\n1. Click on the polygon icon in the menu bar to select the regular or irregular polygon drawing mode.\n",
                     regular);
 
-            icon = loadIcon(IMAGE_PATH_PREFIX + "step-2.png");
-            StyleConstants.setIcon(imageStyle, icon);
-            doc.insertString(doc.getLength(), " ", imageStyle);
+            // icon = loadIcon(IMAGE_PATH_PREFIX + "step-2.png");
+            // StyleConstants.setIcon(imageStyle, icon);
+            // doc.insertString(doc.getLength(), " ", imageStyle);
 
             doc.insertString(doc.getLength(),
                     "\n\n2. Move your mouse cursor on the canvas and click on a point where you want your first polygon focus to be.\n",
@@ -48,10 +53,14 @@ public class DrawPolygonTutorial extends TutorialPanel {
                     "\n\n3. Click on another point where you want your second polygon focus to be.\n\n",
                     regular);
 
-            icon = loadIcon(IMAGE_PATH_PREFIX + "step-3.png");
-            StyleConstants.setIcon(imageStyle, icon);
-            doc.insertString(doc.getLength(), " ", imageStyle);
-        } catch (BadLocationException | InvalidPathException e) {
+            doc.insertString(doc.getLength(),
+                    "Draw irregular polygon\n",
+                    boldLarge);
+
+            // icon = loadIcon(IMAGE_PATH_PREFIX + "step-3.png");
+            // StyleConstants.setIcon(imageStyle, icon);
+            // doc.insertString(doc.getLength(), " ", imageStyle);
+        } catch (BadLocationException e) {
             e.printStackTrace();
         }
     }
