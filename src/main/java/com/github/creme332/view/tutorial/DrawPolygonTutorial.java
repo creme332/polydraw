da0cb610b97c6a69ac4eee63c04cbbd45855cb77
creme332 <65414576+creme332@users.noreply.github.com>
2024-07-05 17:52:21 +0400

    rewrite and add new images

--- src/main/java/com/github/creme332/view/tutorial/DrawPolygonTutorial.java
+++ src/main/java/com/github/creme332/view/tutorial/DrawPolygonTutorial.java
@@ -3,8 +3,6 @@ package com.github.creme332.view.tutorial;
 import static com.github.creme332.utils.IconLoader.loadIcon;
 import static com.github.creme332.utils.IconLoader.loadSVGIcon;
 
-import java.awt.Dimension;
-
 import javax.swing.ImageIcon;
 import javax.swing.text.BadLocationException;
 import javax.swing.text.StyleConstants;
@@ -19,72 +17,82 @@ public class DrawPolygonTutorial extends TutorialPanel {
     private static final TutorialModel TUTORIAL_MODEL = new TutorialModel("Draw Polygon");
     ImageIcon icon;
 
-    public DrawPolygonTutorial() throws InvalidPathException, InvalidIconSizeException {
+    public DrawPolygonTutorial() throws InvalidPathException, InvalidIconSizeException, BadLocationException {
         super(TUTORIAL_MODEL, loadSVGIcon("/icons/regular-polygon.svg", TutorialCard.IMAGE_DIMENSION));
         model.addKeyword("regular");
         model.addKeyword("irregular");
 
-       try {
-            doc.insertString(doc.getLength(),
-                    "In this tutorial you will learn two methods for drawing a polygon using the polygon algorithm.\n\n",
-                    regular);
+        doc.insertString(doc.getLength(),
+                "In this tutorial you will learn how to draw an irregular polygon and a regular polygon.\n\n",
+                regular);
 
-            doc.insertString(doc.getLength(),
-                    "Method 1: Irregular Polygon\n\n",
-                    boldLarge);
+        writeIrregularPolygonSection();
+        writeRegularPolygonSection();
+    }
 
-            doc.insertString(doc.getLength(),
-                    "In this method, each time a point is pressed. It is added to the plottedPoints and when clicked on the first point, then the polygon is drawn.\n",
-                    regular);
+    public void writeIrregularPolygonSection() throws BadLocationException, InvalidPathException {
+        doc.insertString(doc.getLength(),
+                "Irregular Polygon\n\n",
+                boldLarge);
 
-            doc.insertString(doc.getLength(),
-                    "\n\n1. Click on the polygon icon (fifth icon from the left) in the menu bar and select the first option in the dropdown menu.\n",
-                    regular);
+        icon = loadIcon(IMAGE_PATH_PREFIX + "irregular-polygon.gif");
+        StyleConstants.setIcon(imageStyle, icon);
+        doc.insertString(doc.getLength(), " ", imageStyle);
 
-            icon = loadIcon(IMAGE_PATH_PREFIX + "step-1.png");
-            StyleConstants.setIcon(imageStyle, icon);
-            doc.insertString(doc.getLength(), " ", imageStyle);
+        doc.insertString(doc.getLength(),
+                "\n\n1. Click on the polygon icon (fifth icon from the left) in the menu bar and select the first option in the dropdown menu.\n",
+                regular);
 
-            doc.insertString(doc.getLength(),
-                    "\n\n2. Move your mouse cursor to the canvas and select multiple points (more than 2).\n",
-                    regular);
-            doc.insertString(doc.getLength(),
-                    "\n\n4. Once the you designed your polygon that satisfy you then click on the first point again.\n\n",
-                    regular);
+        icon = loadIcon(IMAGE_PATH_PREFIX + "irregular-polygon-icon.png");
+        StyleConstants.setIcon(imageStyle, icon);
+        doc.insertString(doc.getLength(), " ", imageStyle);
 
-        //     icon = loadIcon(IMAGE_PATH_PREFIX + "step-2.png");
-        //     StyleConstants.setIcon(imageStyle, icon);
-        //     doc.insertString(doc.getLength(), " ", imageStyle);
+        doc.insertString(doc.getLength(),
+                "\n\n2. Move your mouse cursor to the canvas and click on multiple points successively. You should see your polygon gradually being built.",
+                regular);
+        doc.insertString(doc.getLength(),
+                "\n\n3. To end drawing you need to click on the first vertex of your polygon again.\n\n",
+                regular);
+    }
 
-            doc.insertString(doc.getLength(),
-                    "\n\nMethod 2: Regular Polygon\n\n",
-                    boldLarge);
-            doc.insertString(doc.getLength(),
-                    "In this method, you will first select the start point of the coordinate on the canvas before entering a fixed value for the number of sizes.\n",
-                    regular);
-            doc.insertString(doc.getLength(),
-                    "\n\n1. Similar to the previous method, begin by clicking on the polygon icon in the menubar but this time choose the second option in the dropdown menu.",
-                    regular);
-            doc.insertString(doc.getLength(),
-                    "\n\n2. After selecting the first point with your cursor, you will be prompted to enter the number of sizes of the polygon. ",
-                    regular);
-            doc.insertString(doc.getLength(),
-                    "The sizes must be an integer greater than 2. \n",
-                    highlightRed);
+    public void writeRegularPolygonSection() throws BadLocationException, InvalidPathException {
+        doc.insertString(doc.getLength(),
+                "Regular Polygon",
+                boldLarge);
 
-            icon = loadIcon(IMAGE_PATH_PREFIX + "step-3.png");
-            StyleConstants.setIcon(imageStyle, icon);
-            doc.insertString(doc.getLength(), " ", imageStyle);
+        doc.insertString(doc.getLength(),
+                "\n\n1. Click on the polygon icon (fifth icon from the left) in the menu bar and select the second option in the dropdown menu.\n",
+                regular);
 
-            doc.insertString(doc.getLength(),
-                    "\n\n6. After entering a valid size, you should see a newly drawn polygon:\n",
-                    regular);
+        icon = loadIcon(IMAGE_PATH_PREFIX + "regular-polygon-icon.png");
+        StyleConstants.setIcon(imageStyle, icon);
+        doc.insertString(doc.getLength(), " ", imageStyle);
 
-            icon = loadIcon(IMAGE_PATH_PREFIX + "step-4.png");
-            StyleConstants.setIcon(imageStyle, icon);
-            doc.insertString(doc.getLength(), " ", imageStyle);
-        } catch (BadLocationException e) {
-            e.printStackTrace();
-        }
+        doc.insertString(doc.getLength(),
+                "\n\n2. On your canvas, click on a pixel to select the center of the polygon.",
+                regular);
+
+        doc.insertString(doc.getLength(),
+                "\n\n3. You will be prompted to enter the number of vertices for your polygon. ",
+                regular);
+        doc.insertString(doc.getLength(),
+                "Your input value must be an integer greater than 2. \n",
+                highlightRed);
+
+        icon = loadIcon(IMAGE_PATH_PREFIX + "step-3.png");
+        StyleConstants.setIcon(imageStyle, icon);
+        doc.insertString(doc.getLength(), " ", imageStyle);
+
+        doc.insertString(doc.getLength(),
+                "\n\n4. You should now see a newly drawn polygon appear. You can control the side length of the polygon by moving your cursor towards or away from the polygon center.",
+                regular);
+
+        doc.insertString(doc.getLength(),
+                "\n\n5. To fix the side length, perform one last click on the canvas.\n",
+                regular);
+
+        icon = loadIcon(IMAGE_PATH_PREFIX + "step-4.png");
+        StyleConstants.setIcon(imageStyle, icon);
+        doc.insertString(doc.getLength(), " ", imageStyle);
     }
 }
