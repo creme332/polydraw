56c0c848832b3a59f5d4cea8cd8f6771f9f58f40
creme332 <65414576+creme332@users.noreply.github.com>
2024-07-12 15:53:48 +0400

    add logo to splash screen

--- src/main/java/com/github/creme332/view/SplashScreen.java
+++ src/main/java/com/github/creme332/view/SplashScreen.java
@@ -3,17 +3,31 @@ package com.github.creme332.view;
 import java.awt.*;
 import javax.swing.*;
 
+import com.github.creme332.utils.IconLoader;
+import com.github.creme332.utils.exception.InvalidPathException;
+
 public class SplashScreen extends JPanel {
     JLabel text = new JLabel("polydraw");
 
     SplashScreen() {
         setLayout(new GridBagLayout());
-
-        text.putClientProperty("FlatLaf.style", "font: 340% $large.font");
         GridBagConstraints gbc = new GridBagConstraints();
+
+        text.putClientProperty("FlatLaf.style", "font: 260% $large.font");
+        gbc.gridx = 0;
+        gbc.gridy = 1;
+        this.add(text, gbc);
+
+        JLabel x = new JLabel();
+        try {
+            x.setIcon(IconLoader.loadIcon("/icons/icosahedron.png"));
+        } catch (InvalidPathException e) {
+            e.printStackTrace();
+            System.exit(0);
+        }
+
         gbc.gridx = 0;
         gbc.gridy = 0;
-
-        this.add(text);
+        this.add(x, gbc);
     }
 }
