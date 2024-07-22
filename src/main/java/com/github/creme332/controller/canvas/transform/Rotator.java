394212680831f99783726a607951b9098b095e01
divyesh000 <divjok28@outlook.com>
2024-07-22 20:49:48 +0400

    Implement rotate about point mode

--- src/main/java/com/github/creme332/controller/canvas/transform/Rotator.java
+++ src/main/java/com/github/creme332/controller/canvas/transform/Rotator.java
@@ -1,6 +1,12 @@
 package com.github.creme332.controller.canvas.transform;
 
 import java.awt.geom.Point2D;
+import javax.swing.ButtonGroup;
+import javax.swing.JLabel;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
 
 import com.github.creme332.model.AppState;
 import com.github.creme332.model.Mode;
@@ -15,18 +21,19 @@ public class Rotator extends AbstractTransformer {
 
     @Override
     public void handleShapeSelection(int shapeIndex) {
-        // get copy of selected shape
+        // Get copy of selected shape
         ShapeWrapper selectedWrapperCopy = canvasModel.getShapeManager().getShapes().get(shapeIndex);
 
-        // TODO: Get rotation details
+        // Request rotation details from the user
+        RotationDetails rotationDetails = requestRotationDetails();
 
-        // perform rotation
-        selectedWrapperCopy.rotate(30, new Point2D.Double(0, 0));
+        // Perform rotation
+        selectedWrapperCopy.rotate(rotationDetails.angle * (rotationDetails.isClockwise ? -1 : 1), rotationDetails.pivot);
 
-        // replace old shape with new one
+        // Replace old shape with new one
         canvasModel.getShapeManager().editShape(shapeIndex, selectedWrapperCopy);
 
-        // repaint canvas
+        // Repaint canvas
         canvas.repaint();
     }
 
@@ -35,4 +42,59 @@ public class Rotator extends AbstractTransformer {
         return getCanvasMode() == Mode.ROTATE_ABOUT_POINT;
     }
 
+    private RotationDetails requestRotationDetails() {
+        JTextField angleField = new JTextField(5);
+        JTextField pivotXField = new JTextField(5);
+        JTextField pivotYField = new JTextField(5);
+
+        JRadioButton clockwiseButton = new JRadioButton("clockwise");
+        JRadioButton counterClockwiseButton = new JRadioButton("counterclockwise");
+        counterClockwiseButton.setSelected(true);
+
+        ButtonGroup directionGroup = new ButtonGroup();
+        directionGroup.add(clockwiseButton);
+        directionGroup.add(counterClockwiseButton);
+
+        JPanel panel = new JPanel();
+        panel.add(new JLabel("Angle:"));
+        panel.add(angleField);
+        panel.add(new JLabel("Pivot X:"));
+        panel.add(pivotXField);
+        panel.add(new JLabel("Pivot Y:"));
+        panel.add(pivotYField);
+        panel.add(counterClockwiseButton);
+        panel.add(clockwiseButton);
+
+        int result = JOptionPane.showConfirmDialog(null, panel, "Rotate About Point",
+                JOptionPane.OK_CANCEL_OPTION, JOptionPane.PLAIN_MESSAGE);
+
+        // Request focus again otherwise keyboard shortcuts will not work
+        canvas.getTopLevelAncestor().requestFocus();
+
+        if (result == JOptionPane.OK_OPTION) {
+            try {
+                double angle = Double.parseDouble(angleField.getText());
+                double pivotX = Double.parseDouble(pivotXField.getText());
+                double pivotY = Double.parseDouble(pivotYField.getText());
+                boolean isClockwise = clockwiseButton.isSelected();
+                return new RotationDetails(angle, new Point2D.Double(pivotX, pivotY), isClockwise);
+            } catch (NumberFormatException e) {
+                // Handle invalid input gracefully, return default rotation details
+                return new RotationDetails(0, new Point2D.Double(0, 0), false);
+            }
+        }
+        return new RotationDetails(0, new Point2D.Double(0, 0), false);
+    }
+
+    private static class RotationDetails {
+        double angle;
+        Point2D pivot;
+        boolean isClockwise;
+
+        RotationDetails(double angle, Point2D pivot, boolean isClockwise) {
+            this.angle = angle;
+            this.pivot = pivot;
+            this.isClockwise = isClockwise;
+        }
+    }
 }
