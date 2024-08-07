51e35a1e322d42664e0d39e849e414373d649eb8
creme332 <65414576+creme332@users.noreply.github.com>
2024-08-07 16:38:57 +0400

    rename ClipperCalculator to LineClipperCalculator

--- src/test/java/com/github/creme332/tests/model/calculator/LineClipperCalculatorTest.java
+++ src/test/java/com/github/creme332/tests/model/calculator/LineClipperCalculatorTest.java
@@ -4,43 +4,43 @@ import static org.junit.Assert.assertArrayEquals;
 
 import org.junit.Test;
 
-import com.github.creme332.model.calculator.ClipperCalculator;
+import com.github.creme332.model.calculator.LineClipperCalculator;
 
-public class ClipperCalculatorTest {
+public class LineClipperCalculatorTest {
 
     @Test
     public void testClipLineCompletelyInside() {
-        double[][] result = ClipperCalculator.clip(2, 2, 4, 4, 1, 1, 5, 5);
+        double[][] result = LineClipperCalculator.clip(2, 2, 4, 4, 1, 1, 5, 5);
         assertArrayEquals(new double[][] { { 2, 2 }, { 4, 4 } }, result);
     }
 
     @Test
     public void testClipLinePartiallyInside() {
-        double[][] result = ClipperCalculator.clip(0, 0, 5, 5, 1, 1, 3, 3);
+        double[][] result = LineClipperCalculator.clip(0, 0, 5, 5, 1, 1, 3, 3);
         assertArrayEquals(new double[][] { { 1, 1 }, { 3, 3 } }, result);
     }
 
     @Test
     public void testClipLineOutside() {
-        double[][] result = ClipperCalculator.clip(-1, -1, 0, 0, 1, 1, 5, 5);
+        double[][] result = LineClipperCalculator.clip(-1, -1, 0, 0, 1, 1, 5, 5);
         assertArrayEquals(result, new double[0][0]);
     }
 
     @Test
     public void testClipLineIntersectingBorder() {
-        double[][] result = ClipperCalculator.clip(0, 0, 6, 6, 1, 1, 5, 5);
+        double[][] result = LineClipperCalculator.clip(0, 0, 6, 6, 1, 1, 5, 5);
         assertArrayEquals(new double[][] { { 1, 1 }, { 5, 5 } }, result);
     }
 
     @Test
     public void testClipLineVertical() {
-        double[][] result = ClipperCalculator.clip(2, 0, 2, 6, 1, 1, 5, 5);
+        double[][] result = LineClipperCalculator.clip(2, 0, 2, 6, 1, 1, 5, 5);
         assertArrayEquals(new double[][] { { 2, 1 }, { 2, 5 } }, result);
     }
 
     @Test
     public void testClipLineHorizontal() {
-        double[][] result = ClipperCalculator.clip(0, 2, 6, 2, 1, 1, 5, 5);
+        double[][] result = LineClipperCalculator.clip(0, 2, 6, 2, 1, 1, 5, 5);
         assertArrayEquals(new double[][] { { 1, 2 }, { 5, 2 } }, result);
     }
 }
